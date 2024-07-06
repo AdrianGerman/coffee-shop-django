a10c68d3e6197aaf392071082406a4adea22d477
AdrianGerman <adrian.german1019@gmail.com>
2024-07-06 14:38:58 -0600

    crispy-tailwind and login form

--- coffee_shop/settings.py
+++ coffee_shop/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crispy_forms',
+    'crispy_tailwind',
     'products',
     'users',
 ]
@@ -123,3 +125,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+CRISPY_ALLOWED_TEMPLATE_PACKS = "tailwind"
+CRISPY_TEMPLATE_PACK = "tailwind"
+LOGIN_REDIRECT_URL = 'list_product'
