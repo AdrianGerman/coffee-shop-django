cfd61e294fa87d12bf5d3f467c4a9412631c9949
AdrianGerman <adrian.german1019@gmail.com>
2024-07-08 10:02:18 -0600

    Django REST Framework use

--- products/views.py
+++ products/views.py
@@ -1,9 +1,11 @@
-from django.shortcuts import render
 from django.views import generic
 from django.urls import reverse_lazy
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 from products.models import Product
 from .forms import ProductForm
+from .serializers import ProductSerializer
 
 
 # Create your views here.
@@ -21,3 +23,13 @@ class ProductListView(generic.ListView):
     model = Product
     template_name = 'products/list_product.html'
     context_object_name = 'products'
+
+
+class ProductListAPI(APIView):
+    authentication_classes = []
+    permission_classes = []
+
+    def get(self, request):
+        products = Product.objects.all()
+        serializer = ProductSerializer(products, many=True)
+        return Response(serializer.data)
